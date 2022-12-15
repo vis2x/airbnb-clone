c25c06d1e31e167b9281a62768af1093e8f43774
vis <48868398+vis2x@users.noreply.github.com>
2022-12-15 23:42:39 +0000

    Added card component

---
+++ components/Card.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Card() {
+    return (
+        <section>
+            <img src="../images/swimmer.png" className="card-img"/>
+            <div className="card-stats">
+                <img src="../images/star.png" className="card-star"/>
+            </div>
+        </section>
+    )
+}
