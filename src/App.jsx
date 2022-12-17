45dafb7aeff286cf7e45c037176865248f3ad60b
vis <48868398+vis2x@users.noreply.github.com>
2022-12-17 21:32:53 +0000

    Mapped data to components

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,30 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+import data from "./data"
+
+
 
 export default function App() {
+
+    const cards = data.map(item => {
+        return  (   
+            <Card 
+                img = {item.coverImg}
+                rating={item.stats.rating}
+                reviewCount={item.stats.reviewCount}
+                location={item.location}
+                title={item.title}
+                price={item.price}
+            />
+        )
+    })
+
     return (
         <div>
             <Navbar />
             <Hero />
-            <Card 
-                img="swimmer.png"
-                rating={5.0}
-                reviewCount={6}
-                country="England"
-                title="Life lessons with ..."
-                price={136}
-            />
+            {cards}
         </div>
     )
 }
