88cbf3af62e4cba7abc36cd7cbb2b32d58a67106
vis <48868398+vis2x@users.noreply.github.com>
2022-12-17 20:37:49 +0000

    Changed image and component folder locations

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react"
-import Navbar from "../components/Navbar"
-import Hero from "../components/Hero"
-import Card from "../components/Card"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
 
 export default function App() {
     return (
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Card(props) {
     return (
         <div className="card">
-            <img src={`../images/${props.img}`} className="card--img" />
+            <img src={`./images/${props.img}`} className="card--image" />
             <div className="card--stats">
                 <img src="../images/star.png" className="card--star" />
                 <span>{props.rating}</span>
