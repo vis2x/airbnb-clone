57956882996b09ad945ea54888d5a4bbd262d513
vis <48868398+vis2x@users.noreply.github.com>
2022-12-20 01:20:37 +0000

    Minor adjustments - refactored code

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -11,11 +11,11 @@ export default function Card(props) {
     return (
         <div className="card">
         {badgeText && <div className="card--badge">{badgeText}</div>}
-            <img src={`../images/${props.img}`} className="card--image" />
+            <img src={`../images/${props.coverImg}`} className="card--image" />
             <div className="card--stats">
                 <img src="../images/star.png" className="card--star" />
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) • </span>
+                <span>{props.stats.rating}</span>
+                <span className="gray">({props.stats.reviewCount}) • </span>
                 <span className="gray">{props.location}</span>
             </div>
             <p>{props.title}</p>
